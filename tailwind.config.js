8ccf0d432b29fb48927fa6fd84e44b22bb734a9e
Fariha5128 <fimeem49@gmail.com>
2022-04-26 19:45:42 +0600

    2nd commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@ module.exports = {
   content: ['./*html'],
   theme: {
     screens: {
-      sm: '480px',
-      md:'768px',
-      lg:'976px',
+      sm: '540px',
+      md: '820px',
+      lg: '976px',
       xl: '1440px'
     },
     extend: {
